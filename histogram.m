de5b14beec9ea22b4c8a51647da6ba8fff4cd3a9
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-11-04 15:24:16 -0200

    Poster 2.0

--- histogram.m
+++ histogram.m
@@ -1,8 +1,14 @@
-function histogram(filename)
+function histogram(filename, titulo)
 	pid = fopen(filename, "r", "native");
 	dados = fscanf(pid, "%f");
+	length(filename);
 	hist(dados, 20);
+	title(titulo);
+	grid();
+	box();
+	xlabel("Tempo(ms)");
+	ylabel("Quantidade de Threads");
 	das = substr(filename, 1, length(filename)-4);
 	oi = strcat(das,"_histo.jpg");
-	print(oi, "-djpg");
+	print -F:45 -djpeg -r80 oi;
 
